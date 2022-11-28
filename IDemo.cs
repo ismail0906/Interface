95ea481e6ca1c4aed43c49fb70b7852c4c776381
ASUS <ASUS@ISMAIL>
2022-11-28 22:42:30 +0300

    Add project files.

---
+++ IDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Interface
+{
+    public interface IDemo
+    {
+
+        // interface (arayüzler) => birden fazla yapıdan miras almamızı sağyalan yapılardır.. (multiple inheritance)
+
+        // interfaceler soyut varlıklardır. sadece miras vermek için tanımlanır...
+
+        // abstract classlara benzler ancak abstract yapılarda tek kalıtım, interfacelerde çoklu kalıtım yapılır...
+
+        // interfacelerde field tanımlanamaz...
+        // interface üyelerinin gövdeleri yazılamayabilir.. ( // 8.0 ile gövdeli üyeler tanımlanabilir
+
+        public void Deneme() 
+        {
+            Console.WriteLine("Deneme");
+        }
+
+        public void Message();
+    }
+
+    public class Demo : IDemo 
+    {
+        public void Message() 
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _7_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
